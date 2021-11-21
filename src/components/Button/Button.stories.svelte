0e106af3e9eaf8915c394f9f33bef5740191f8ff
wesleynepo <wesleynepomuceno1@gmail.com>
2021-11-21 15:37:37 -0300

    feat: create Button component

---
+++ src/components/Button/Button.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import Button from "./Button.svelte";
+</script>
+  
+  <!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+  <!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta
+  title='Button'
+  component=Button
+/>
+
+<Template let:args>
+  <Button
+   {...args}
+  />
+</Template>
+
+<Story
+  name="Normal"
+  args={
+    {
+      label: 'Login'
+    }
+  }
+/>
+
---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let label:string;
+</script>
+
+<button>
+    <span>
+        {label}
+    </span>
+</button>
+
+
+<style>
+    button {
+        background-color: #20DF7F;
+        border-radius: 0.625rem;
+        width: 18.75rem;
+        height: 2.813rem;
+        border-width: 0;
+        box-shadow: 0 0.25rem 0.25rem rgba(0, 0, 0, 0.3);
+    }
+
+    span {
+        line-height: 1rem;
+        color:#224957;
+        font-size: 1rem;
+        font-weight: bold;
+    }
+</style>
