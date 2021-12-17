5c6556121914b205732b32c0233cd0fab7af64ed
wesleynepo <wesleynepomuceno1@gmail.com>
2021-12-16 21:28:08 -0300

    feat: LayoutFooter

---
+++ src/components/LayoutFooter/LayoutFooter.stories.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import LayoutFooter from "./LayoutFooter.svelte";
+</script>
+  
+  <!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+  <!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta
+  title='LayoutFooter'
+  component=LayoutFooter
+/>
+
+<Template let:args>
+  <LayoutFooter
+   {...args}
+  />
+</Template>
+
+<Story
+  name="Normal"
+/>
+
---
+++ src/components/LayoutFooter/LayoutFooter.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+</script>
+
+<div>
+    <svg viewBox="0 0 1280 111" preserveAspectRatio="xMinYMin meet">
+        <path
+            fill="#20DF7F"
+            fill-opacity="0.8"
+            d="m -1 0 l 53 4.1705 c 54 4.1705 160 12.5116 267 26.6272 c 107 14.4364 213 34.9682 320 39.1387 c 107 4.1705 213 -8.341 320 -12.1907 c 107 -4.1706 213 0 267 1.9248 l 53 1.9249 v 49.4046 h -53 c -54 0 -160 0 -267 0 c -107 0 -213 0 -320 0 c -107 0 -213 0 -320 0 c -107 0 -213 0 -267 0 h -53 v -111 z"
+        />
+    </svg>
+    <svg viewBox="0 0 1280 111" preserveAspectRatio="xMinYMin meet">
+        <path
+            fill="#224957"
+            fill-opacity="0.8"
+            d="m 0 44.4 l 42.6667 8.88 c 42.6666 8.88 128.0003 26.64 213.3333 22.2 c 85.333 -4.44 170.667 -31.08 256 -48.84 c 85.333 -17.76 170.667 -26.64 256 -26.64 c 85.333 0 170.667 8.88 256 24.42 c 85.33 15.54 170.67 37.74 213.33 48.84 l 42.67 11.1 v 26.64 h -42.67 c -42.66 0 -128 0 -213.33 0 c -85.333 0 -170.667 0 -256 0 c -85.333 0 -170.667 0 -256 0 c -85.333 0 -170.667 0 -256 0 c -85.333 0 -170.6667 0 -213.3333 0 h -42.6667 v -66.6 z"
+        />
+    </svg>
+</div>
+
+<style>
+    svg {
+        display: inline-block;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+    }
+
+    div {
+        display: inline-block;
+        position: relative;
+        width: 100%;
+        padding-bottom: 100%;
+        vertical-align: middle;
+        overflow: hidden;
+    }
+</style>
